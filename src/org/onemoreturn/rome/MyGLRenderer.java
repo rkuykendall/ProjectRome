cce3bf0485102a4cb0b36e2a25c1bc5e250cc615
Robert Kuykendall <robert@rkuykendall.com>
2013-12-06 17:57:43 -0500

    Initial commit.

---
+++ src/org/onemoreturn/rome/MyGLRenderer.java
@@ -0,0 +1,67 @@
+package org.onemoreturn.rome;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+	// private HexTile mHex;
+    private Tile mTile;
+    private float mAngle;
+	
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set the background frame color
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+       mTile = new Tile();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+        // Draw background color
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        // Set GL_MODELVIEW transformation mode
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();   // reset the matrix to its default state
+
+        // When using GL_MODELVIEW, you must set the view point
+        GLU.gluLookAt(gl, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Draw tile
+        mTile.draw(gl);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Adjust the viewport based on geometry changes
+        // such as screen rotations
+        gl.glViewport(0, 0, width, height);
+
+        // make adjustments for screen ratio
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
+        gl.glLoadIdentity();                        // reset the matrix to its default state
+        gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
+    }
+
+    /**
+     * Returns the rotation angle of the triangle shape (mTriangle).
+     *
+     * @return - A float representing the rotation angle.
+     */
+    public float getAngle() {
+        return mAngle;
+    }
+
+    /**
+     * Sets the rotation angle of the triangle shape (mTriangle).
+     */
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+}
---
+++ src/org/onemoreturn/rome/Tile.java
@@ -0,0 +1,74 @@
+package org.onemoreturn.rome;
+
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * A two-dimensional triangle for use as a drawn object in OpenGL ES 1.0/1.1.
+ */
+public class Tile {
+    private final FloatBuffer vertexBuffer;
+
+    // number of coordinates per vertex in this array
+    static final int COORDS_PER_VERTEX = 3;
+
+    private float vertices[] = {     // 0.0f,   0.0f, 0.0f,    //center
+    		0.0f,   1.0f, 0.0f,    // top
+           -1.0f,   0.5f, 0.0f,    // left top
+           -1.0f,  -0.5f, 0.0f,    // left bottom
+            0.0f,  -1.0f, 0.0f,    // bottom
+            1.0f,  -0.5f, 0.0f,    // right bottom
+            1.0f,  0.5f, 0.0f,     // right top
+    		0.0f,   1.0f, 0.0f,    // top
+    };
+
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 0.0f };
+
+    /**
+     * Sets up the drawing object data for use in an OpenGL ES context.
+     */
+    public Tile() {
+        // initialize vertex byte buffer for shape coordinates
+        ByteBuffer bb = ByteBuffer.allocateDirect(
+        // (number of coordinate values * 4 bytes per float)
+        vertices.length * 4);
+        // use the device hardware's native byte order
+        bb.order(ByteOrder.nativeOrder());
+
+        // create a floating point buffer from the ByteBuffer
+        vertexBuffer = bb.asFloatBuffer();
+        // add the coordinates to the FloatBuffer
+        vertexBuffer.put(vertices);
+        // set the buffer to read the first coordinate
+        vertexBuffer.position(0);
+    }
+
+    /**
+     * Encapsulates the OpenGL ES instructions for drawing this shape.
+     *
+     * @param gl - The OpenGL ES context in which to draw this shape.
+     */
+    public void draw(GL10 gl) {
+        // Since this shape uses vertex arrays, enable them
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+        // draw the shape
+        gl.glColor4f( // set color:
+                color[0], color[1],
+                color[2], color[3]);
+        gl.glVertexPointer( // point to vertex data:
+                COORDS_PER_VERTEX,
+                GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glDrawArrays(    // draw shape:
+                GL10.GL_TRIANGLE_FAN, 0,
+                vertices.length / COORDS_PER_VERTEX);
+
+        // Disable vertex array drawing to avoid
+        // conflicts with shapes that don't use it
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+    }
+}
