abc677985685070afde708777c72c4d62ce3ae4c
Robert Kuykendall <robert@rkuykendall.com>
2015-09-25 20:24:13 -0400

    Start move to 2d OpenGL.

---
+++ out/production/ProjectRome/README.md
@@ -0,0 +1,37 @@
+Project: Rome
+=============
+
+A 4X game for Android (Explore, Expand, Exploite, Exterminate)
+
+
+
+  What is Project: Rome?
+  ---------------------
+
+  Project: Rome is a multi-platform 4x Turn Based Strategy game. 
+  Drawing from such titles as Sid Meir's Alpha Centauri TM ®, Civilization TM® series, as 
+  well as influences from Warcraft TM® & the Ceaser TM® series. 
+  
+  Documentation
+  -------------
+
+  The documentation available as of the date of this release is
+  included in HTML format in the XXXXXXXXX directory.  The most
+  up-to-date documentation can be found at:
+  https://github.com/rkuykendall/ProjectRome.
+  
+  Installation
+  ------------
+
+  Please see the file called INSTALL.  Platform specific notes can be
+  found in README.platforms.
+
+  Licensing
+  ---------
+
+  Please see the file called LICENSE.
+  
+  Contacts
+  --------
+
+
--- src/org/onemoreturn/rome/MainActivity.java
+++ src/org/onemoreturn/rome/MainActivity.java
@@ -3,38 +3,51 @@ package org.onemoreturn.rome;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
-    private GLSurfaceView mGLView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	// Our OpenGL Surfaceview
+	private GLSurfaceView glSurfaceView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		// Turn off the window's title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        // Super
+		super.onCreate(savedInstanceState);
+		
+		// Fullscreen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // We create our Surfaceview for our OpenGL here.
+        glSurfaceView = new GLSurf(this);
+        
+        // Set our view.	
+		setContentView(R.layout.activity_main);
+		
+		// Retrieve our Relative layout from our main layout we just set to our view.
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.gamelayout);
+        
+        // Attach our surfaceview to our relative layout from our main layout.
+        RelativeLayout.LayoutParams glParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        layout.addView(glSurfaceView, glParams);
+	}
 
-        // Create a GLSurfaceView instance and set it
-        // as the ContentView for this Activity
-        mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		glSurfaceView.onPause();
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // The following call pauses the rendering thread.
-        // If your OpenGL application is memory intensive,
-        // you should consider de-allocating objects that
-        // consume significant memory here.
-        mGLView.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // The following call resumes a paused rendering thread.
-        // If you de-allocated graphic objects for onPause()
-        // this is a good place to re-allocate them.
-        mGLView.onResume();
-    }
-
+	@Override
+	protected void onResume() {
+		super.onResume();
+		glSurfaceView.onResume();
+	}
 
 }
