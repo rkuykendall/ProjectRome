3342a87c743247a5a5429e521ef858251876036e
Robert Kuykendall <robert@rkuykendall.com>
2015-10-23 21:37:29 -0400

    Add different type types to tile.

--- src/org/onemoreturn/rome/logic/Tile.java
+++ src/org/onemoreturn/rome/logic/Tile.java
@@ -13,12 +13,20 @@ public class Tile {
     public int x;
     public int y;
 
+    public int[] tileTypes = new int[] {
+            R.drawable.hex_grass_grid,
+            R.drawable.hex_sand_grid,
+            R.drawable.hex_ice_grid
+    };
+
     public Tile(int x, int y, Context mContext)
     {
         this.x = x;
         this.y = y;
 
-        Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.hex_sand_grid);
+        int texture = tileTypes[(x+y)%tileTypes.length];
+
+        Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), texture);
         mSprite = new Sprite(.25f*x + (y%2)*.125f - .7f, .145f*y, .25f, .25f, bmp);
         // bmp.recycle();
     }
